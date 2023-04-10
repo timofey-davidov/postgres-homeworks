5a17b1769342a7a82f73ddc0da665fc6efa43d29
timofey-davidov <timofeydavidov95@gmail.com>
2023-04-10 07:44:56 +0300

    finishe homework1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id INT PRIMARY KEY,
+	first_name VARCHAR(10) NOT NULL,
+	last_name VARCHAR(10) NOT NULL,
+	title VARCHAR(30),
+	birth_date DATE,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id VARCHAR(5) PRIMARY KEY,
+	company_name VARCHAR(255),
+	contact_name VARCHAR(255)
+);
+
+CREATE TABLE orders
+(
+	order_id INT PRIMARY KEY,
+	customer_id VARCHAR(5) REFERENCES customers(customer_id),
+	employee_id INT REFERENCES employees(employee_id) NOT NULL,
+	order_date DATE,
+	ship_city VARCHAR(20)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,41 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+
+with open("north_data/employees_data.csv", newline='') as csv_file:
+    reader = csv.DictReader(csv_file, delimiter=",")
+    conn = psycopg2.connect(host="localhost", database="north", user="postgres", password="123456")
+    cur = conn.cursor()
+    counter = 0
+    for row in reader:
+        counter += 1
+        cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)",
+                    (counter, row["first_name"], row["last_name"], row["title"], row["birth_date"], row["notes"]))
+        conn.commit()
+    cur.close()
+    conn.close()
+
+with open("north_data/customers_data.csv", newline='') as csv_file:
+    reader = csv.DictReader(csv_file, delimiter=",")
+    conn = psycopg2.connect(host="localhost", database="north", user="postgres", password="123456")
+    cur = conn.cursor()
+    for row in reader:
+        cur.execute("INSERT INTO customers VALUES (%s, %s, %s)",
+                    (row["customer_id"], row["company_name"], row["contact_name"]))
+        conn.commit()
+    cur.close()
+    conn.close()
+
+with open("north_data/orders_data.csv", newline='') as csv_file:
+    reader = csv.DictReader(csv_file, delimiter=",")
+    conn = psycopg2.connect(host="localhost", database="north", user="postgres", password="123456")
+    cur = conn.cursor()
+    for row in reader:
+        cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)",
+                    (row["order_id"], row["customer_id"], row["employee_id"], row["order_date"], row["ship_city"]))
+        conn.commit()
+    cur.close()
+    conn.close()
+
+
